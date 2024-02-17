6dda537d609e638760fe3259bd3b2874ae890ef1
Mstric27 <134113469+Mstric27@users.noreply.github.com>
2024-02-17 09:17:55 -0500

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -7,12 +7,7 @@ import * as firebase from "firebase/compat";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDR6jSEeiGYd8cle2sYVKbzBWdc99TXtNM",
-  authDomain: "budgetaway.firebaseapp.com",
-  projectId: "budgetaway",
-  storageBucket: "budgetaway.appspot.com",
-  messagingSenderId: "854485873301",
-  appId: "1:854485873301:web:8d2f9b4708845e0282619c"
+  // ADD FIREBASE INFO HERE
 };
 
 // Initialize Firebase
